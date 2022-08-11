10bcde9b4b1515ac216247ce022c6057ffacdbae
roilhi <roilhi@gmail.com>
2022-08-11 16:23:49 -0700

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace ConsultaCuentasUNIENS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbLastName = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbClose = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbCarrera = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbTurno = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbCuatri = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tbMatr = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.tbClave = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbLastName
+            // 
+            this.tbLastName.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbLastName.Location = new System.Drawing.Point(279, 108);
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(462, 27);
+            this.tbLastName.TabIndex = 0;
+            this.tbLastName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbLastName_KeyDown);
+            // 
+            // tbName
+            // 
+            this.tbName.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbName.Location = new System.Drawing.Point(279, 156);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(462, 27);
+            this.tbName.TabIndex = 1;
+            this.tbName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbName_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Navy;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(34, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Apellidos del estudiante:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Navy;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(34, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(235, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nombres del estudiante:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Navy;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(233, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(284, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Consulta de cuentas digitales";
+            // 
+            // lbClose
+            // 
+            this.lbClose.AutoSize = true;
+            this.lbClose.BackColor = System.Drawing.Color.Navy;
+            this.lbClose.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbClose.ForeColor = System.Drawing.Color.White;
+            this.lbClose.Location = new System.Drawing.Point(743, 9);
+            this.lbClose.Name = "lbClose";
+            this.lbClose.Size = new System.Drawing.Size(23, 23);
+            this.lbClose.TabIndex = 5;
+            this.lbClose.Text = "X";
+            this.lbClose.Click += new System.EventHandler(this.lbClose_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ConsultaCuentasUNIENS.Properties.Resources.UNIENSBLANCO__1_;
+            this.pictureBox1.Location = new System.Drawing.Point(24, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(191, 62);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Navy;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(34, 211);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "email:";
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbEmail.Location = new System.Drawing.Point(118, 211);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(623, 27);
+            this.tbEmail.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Navy;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(34, 261);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 23);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "contraseña:";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPassword.Location = new System.Drawing.Point(159, 261);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(226, 27);
+            this.tbPassword.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Navy;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(20, 368);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 23);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "carrera:";
+            // 
+            // tbCarrera
+            // 
+            this.tbCarrera.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCarrera.Location = new System.Drawing.Point(108, 364);
+            this.tbCarrera.Name = "tbCarrera";
+            this.tbCarrera.Size = new System.Drawing.Size(633, 27);
+            this.tbCarrera.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Navy;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(41, 312);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 23);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "turno:";
+            // 
+            // tbTurno
+            // 
+            this.tbTurno.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTurno.Location = new System.Drawing.Point(108, 312);
+            this.tbTurno.Name = "tbTurno";
+            this.tbTurno.Size = new System.Drawing.Size(107, 27);
+            this.tbTurno.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Navy;
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(233, 313);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(130, 23);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "cuatrimestre:";
+            // 
+            // tbCuatri
+            // 
+            this.tbCuatri.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCuatri.Location = new System.Drawing.Point(369, 309);
+            this.tbCuatri.Name = "tbCuatri";
+            this.tbCuatri.Size = new System.Drawing.Size(85, 27);
+            this.tbCuatri.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Navy;
+            this.label9.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(403, 260);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(104, 23);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "matrícula:";
+            // 
+            // tbMatr
+            // 
+            this.tbMatr.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbMatr.Location = new System.Drawing.Point(513, 260);
+            this.tbMatr.Name = "tbMatr";
+            this.tbMatr.Size = new System.Drawing.Size(228, 27);
+            this.tbMatr.TabIndex = 18;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Navy;
+            this.label10.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(474, 309);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(68, 23);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "clave:";
+            // 
+            // tbClave
+            // 
+            this.tbClave.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbClave.Location = new System.Drawing.Point(548, 311);
+            this.tbClave.Name = "tbClave";
+            this.tbClave.Size = new System.Drawing.Size(85, 27);
+            this.tbClave.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
+            this.ClientSize = new System.Drawing.Size(778, 442);
+            this.Controls.Add(this.tbClave);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.tbMatr);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.tbCuatri);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.tbTurno);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tbCarrera);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbClose);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.tbLastName);
+            this.ForeColor = System.Drawing.Color.Navy;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbLastName;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbClose;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbCarrera;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbTurno;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tbCuatri;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox tbMatr;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox tbClave;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+
+namespace ConsultaCuentasUNIENS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lbClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ingresa los apellidos del estudiante");
+            tbLastName.Select();
+            tbLastName.Focus();
+        }
+
+        private void tbLastName_KeyDown(object sender, KeyEventArgs e) 
+        { 
+            if (e.KeyCode == Keys.Enter)
+            {
+                tbLastName.Text= tbLastName.Text.Trim().ToUpper();
+                MessageBox.Show("Ingresa los nombres del estudiante");
+                tbName.Select();
+                tbName.Focus();
+            } 
+        }
+        private string ClaveCarrera(string clave) 
+        {
+            string Carrera = "";
+            if(clave == "LAITE") 
+            {
+                Carrera = "LICENCIADO EN ADMINISTRACIÓN DE EMPRESAS Y LOGÍSTICA";
+            }
+            else if (clave == "LCEITE") 
+            {
+                Carrera = "LICENCIADO EN CIENCIAS DE LA EDUCACION";
+            }
+            else if (clave == "IDITE") 
+            {
+                Carrera = "INGENIERIA INDUSTRIAL";
+            }
+            else if (clave == "LDITE") 
+            {
+                Carrera = "LICENCIADO EN DERECHO";
+            }
+            else if (clave == "LFITE") 
+            {
+                Carrera = "LICENCIADO EN FILOSOFÍA";
+            }
+            else if (clave == "LMAITE") 
+            {
+                Carrera = "LICENCIADO EN MEDIOS AUDIOVISUALES";
+            }
+            else if (clave == "LNITE") 
+            {
+                Carrera = "LICENCIADO EN NEGOCIOS INTERNACIONALES";
+            }
+            else if (clave == "LCRITE") 
+            {
+                Carrera = "LICENCIADO EN CRIMINOLOGÍA";
+            }
+            return Carrera;
+        }
+        private void tbName_KeyDown(object sender, KeyEventArgs e) 
+        { 
+            if (e.KeyCode == Keys.Enter) 
+            {
+                tbName.Text = tbName.Text.Trim().ToUpper();
+                string wName = tbLastName.Text + " " + tbName.Text;
+                var settings = MongoClientSettings.FromConnectionString("mongodb+srv://uniens:uniens22@cluster0.ex5nn.mongodb.net/?retryWrites=true&w=majority?connect=replicaSet");
+                var client = new MongoClient(settings);
+                var database = client.GetDatabase("studENS");
+                var collection = database.GetCollection<BsonDocument>("infoMails");
+                var filter = Builders<BsonDocument>.Filter.Eq("wholeName", wName);
+                var BsonDoc = collection.Find(filter).FirstOrDefault();
+                try
+                {
+                    if (BsonDoc != null) 
+                    {
+                        string email = BsonDoc["mail"].ToString();
+                        string pass = BsonDoc["password"].ToString();
+                        string matr = BsonDoc["matInt"].ToString();
+                        string turno = BsonDoc["turno"].ToString();
+                        string cuatr = BsonDoc["cuatri"].ToString();
+                        string clave = BsonDoc["carrera"].ToString();
+                        tbEmail.Text = email;
+                        tbPassword.Text = pass;
+                        tbMatr.Text = matr;
+                        tbTurno.Text = turno;
+                        tbCuatri.Text = cuatr;
+                        tbClave.Text = clave;
+                        tbCarrera.Text = ClaveCarrera(clave);
+                    }
+                    else 
+                    {
+                        MessageBox.Show("El usuario no existe, favor de verificar");
+                    }
+                }
+                catch 
+                {
+                    MessageBox.Show("Error con la conexión a la base de datos");
+                }
+            }
+        }
+    }
+}
